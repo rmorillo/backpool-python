e42d3ba8300955f59e2d5aa5c0151ea411278931
ronaldomorillo <ronaldo.morillo@yahoo.com>
2017-08-24 13:12:02 +0800

    Make list_back count parameter optional as well

--- source/lookbehindpool.py
+++ source/lookbehindpool.py
@@ -65,7 +65,7 @@ class LookBehindPool:
             raise Exception("Pool index is out of range.  You can only read back maximum of {} items. \
                 You can use 'is_valid_index()' or 'length' to validate your 'count' parameter".format(self._length))
 				
-    def list_back(self, count, item_lambda=None):
+    def list_back(self, count=None, item_lambda=None):
         return list(self.read_back(count, item_lambda))
 
 # Internal methods
